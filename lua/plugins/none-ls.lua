9562532ccbf8f8e991e595ddd13407f5433a587c
Silence <silence178@proton.me>
2025-09-28 17:21:34 +0800

    a

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -16,12 +16,14 @@ return {
 		install("stylua")
 		install("clang-format")
         install("gersemi")
+        install("black")
 		local null_ls = require("null-ls")
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.clang_format,
 				null_ls.builtins.formatting.gersemi,
+				null_ls.builtins.formatting.black,
 			},
 		})
 	end,
